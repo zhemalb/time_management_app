fde6c9e3aeb3730753af34224ed4591b675c8058
Firstname Secondname <101018980+zhemalb@users.noreply.github.com>
2024-07-21 20:14:49 +0300

    Add files via upload
    /tasks

--- time_management/time_management.py
+++ time_management/time_management.py
@@ -5,6 +5,7 @@ import reflex as rx
 
 from .pages.footer import render_footer
 from .pages.register import register_page
+from .pages.task_creation import tasks_page
 
 from .utils.state.auth import AuthState
 
@@ -17,13 +18,18 @@ def index():
     return render_footer()
 
 
-@rx.page("/test")
+@rx.page("/register")
 def login():
     return register_page()
 
 
+@rx.page("/tasks")
+def aaa():
+    return tasks_page()
+
+
 app = rx.App(
     stylesheets=[
-        "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swa",
+        "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
     ],
 )
